55c17b6fba49618716fc1bef412399a4fe204bc5
VictorRuizChambi <victor100490sistem@gmail.com>
2019-04-11 17:37:28 -0500

    se eliminana submodulos de devCode

--- devCode/AngularJs Basico/Proyectos/Proyecto_01/common/services/categories/categoriesService.js
+++
@@ -1,9 +0,0 @@
-(function(){
-   "use strict";
-   angular.module("common.services")
-       .factory("categoriesService",categoriesService);
-   function categoriesService($http){
-       //esto retorna una promesa
-       return $http.get("/api/categorias");
-   }
-}());
--- devCode/AngularJs Basico/Proyectos/Proyecto_01/common/services/news/newsResource.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    "use strict";
-    angular.module("common.services")
-        .factory("newsResource",newsResource);
-
-    function newsResource($resource){
-        return $resource("/api/noticias/:idNoticia");
-    }
-}());
